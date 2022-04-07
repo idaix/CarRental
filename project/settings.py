ade803a84301f07e34e2c7de43c65ffdeda93f57
daishek <daichekkal@gmail.com>
2022-04-07 10:30:09 +0100

    accounts

--- project/settings.py
+++ project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -130,3 +131,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+
+# REDIRECT_URL
+LOGIN_REDIRECT_URL = '/agency/dashboard'
