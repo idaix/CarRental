0389b7297990fe8b7ebd91679990c3ac8e4ae166
Slimane <80587317+slimanesedrati@users.noreply.github.com>
2022-05-01 01:58:46 +0100

    Update urls.py

--- project/urls.py
+++ project/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from accounts import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
@@ -11,4 +12,24 @@ urlpatterns = [
     path('', include('app.urls')),
     # path('vehicle/', include('vehicle.urls')),
     path('agency/', include('agency.urls')),
+    path('password-reset/',
+        auth_views.PasswordResetView.as_view(
+            template_name='registration/password_reset.html'
+        ),
+        name='password_reset'),
+    path('password-reset/done/',
+        auth_views.PasswordResetDoneView.as_view(
+            template_name='registration/password_reset_done.html'
+        ),
+        name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name='registration/password_reset_confirm.html'
+        ),
+        name='password_reset_confirm'),
+    path('password-reset-complete/',
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name='registration/password_reset_complete.html'
+        ),
+        name='password_reset_complete'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
