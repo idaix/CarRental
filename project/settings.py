fcc125c7cc4844ef6af8e3535e1fd57b85a5697e
daishek <daichekkal@gmail.com>
2022-05-08 11:22:01 +0100

    reset  password

--- project/settings.py
+++ project/settings.py
@@ -160,5 +160,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'Your Email'
-EMAIL_HOST_PASSWORD = 'Your password'
+EMAIL_HOST_USER = 'carentalgeria@gmail.com'
+EMAIL_HOST_PASSWORD = 'vMzkH9WBBRy5a9g'
