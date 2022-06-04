3fadcc61548dcdd4c54bb20e87dfbd46e89f7f8e
daishek <daichekkal@gmail.com>
2022-06-04 22:27:14 +0100

    update hosts

--- project/settings.py
+++ project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-v4a=8lw33nvne6m0^j+bh&q#iri_00&-_b+2%ab+bhl5091eqi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['autorent.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
